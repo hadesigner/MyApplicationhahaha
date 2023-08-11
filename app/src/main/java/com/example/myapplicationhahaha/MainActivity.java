7d2ec17a7bfff70b293f75c135632652bd028075
hadesigner <56531726+hadesigner@users.noreply.github.com>
2023-08-11 14:36:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationhahaha/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplicationhahaha;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    RadioGroup rg;
+    RadioButton rb1, rb2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rg = findViewById(R.id.radioGroup01);
+        rb1 = findViewById(R.id.radio01);
+        rb2 = findViewById(R.id.radio02);
+
+
+    }
+
+    public void onSelectButtonClicked(View view) {
+        if (rb1.isChecked() == true) {
+            Toast.makeText(getApplicationContext(), "남성", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "여성", Toast.LENGTH_LONG).show();
+        }
+    }
+}
